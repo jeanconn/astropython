f6ab6a56eb65c231827c14b842de88f43ff24277
amanXCalibre <amanjhunjhunwala@outlook.com>
2015-05-28 18:14:04 +0530

    Population Script-Completed for Tutorials
    Tagging remains

--- astropython/populate.py
+++ astropython/populate.py
@@ -1,5 +1,6 @@
 import os
 import json
+from slugify import slugify
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'astropython.settings')
 
 import django
@@ -10,8 +11,20 @@ from blog.models import Event,MarkdownPost,WYSIWYGPost
 from packages.models import MarkdownInput,WYSIWYGInput
 from category.models import Category
 
+from django.contrib.auth.models import User
+
+def initialize():
+    c=Category.objects.get_or_create(title="Uncategorized")
+    return c
+
+
 def populate_tutorials():
     global base
+    author=""
+    title=""
+    desc=""
+    tags=""
+    date=""
     path_localdata=os.path.join(base,'Tutorials')
     try:
         no=len(os.walk(path_localdata).next()[2])
@@ -20,6 +33,7 @@ def populate_tutorials():
                 data=json.load(data_file)
                 s=""
                 for name in data:
+                    print name
                     if (name=='tags'):
                         s1=""
                         for items in data[name]:
@@ -27,29 +41,39 @@ def populate_tutorials():
                             s=s1[1:]
                     else:
                         s=data[name]
-                    if(name.encode('ascii','ignore')=='author'):
+                    if(name=='author'):
                         author=s.encode('ascii','ignore')
-                    elif(name.encode('ascii','ignore')=='title'):
+                    elif(name=='title'):
                         title=s.encode('ascii','ignore')
-                    elif(name.encode('ascii','ignore')=='description'):
+                    elif(name=='description'):
                         desc=s.encode('ascii','ignore')
-                    elif(name.encode('ascii','ignore')=='tags'):
+                    elif(name=='tags'):
                         tags=s.encode('ascii','ignore')
-                    elif(name.encode('ascii','ignore')=='date_published'):
+                    elif(name=='date_published'):
                         date=s.encode('ascii','ignore')
-                    #t=WYSIWYGTutorial.objects.get_or_create(category='Uncategorized')
-                    t.save()
-                    return t
+                t=WYSIWYGTutorial(title=title,body=desc,slug=slugify(title))
+                u=User.objects.get_or_create(username=author)
+                c=Category.objects.get_or_create(title='Uncategorized')
+                t.save()
+                if(u[1]==True):
+                    u[0].save()
+                if(c[1]==True):
+                    c[0].save()
+                t.authors.add(u[0])
+                t.categories.add(c[0])
+                t.save()
+                print("Entered element!")
     except IOError:
         print("IOError encountered!")
 
 # Start execution here!
 if __name__ == '__main__':
-    _1orig_path=os.getcwd()
-    _orig_path=os.path.dirname(_1orig_path)
+    _temp_orig_path=os.getcwd()
+    _orig_path=os.path.dirname(_temp_orig_path)
     base=os.path.join(_orig_path,'ported_data')
     print "Starting Astropython population script..."
     opt=raw_input("Do you want to populate the tutorials? (y/n):")
+    initialize()
     if opt=='y':
         populate_tutorials()
     """
