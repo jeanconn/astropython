63c5b8b5e990780a12c6d47983dca1ea835295e5
amanXCalibre <amanjhunjhunwala@outlook.com>
2015-05-29 19:50:35 +0530

    Added comments to populate

--- astropython/populate.py
+++ astropython/populate.py
@@ -1,3 +1,6 @@
+"""
+This script populated the database initially with the old ported data stored with the repo
+"""
 import os
 import json
 import datetime
@@ -5,6 +8,7 @@ import random
 from slugify import slugify
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'astropython.settings')
 
+#Setup Django and add all dependencies and code automatically
 import django
 django.setup()
 
@@ -15,33 +19,36 @@ from category.models import Category
 
 from django.contrib.auth.models import User
 
+"""
+initialize() will be used to create Groups and other general entities in the future
+"""
 def initialize():
     pass
-    #Use this to create groups and other general stuff in the future
 
-
+"""
+Main population script that parses JSON and stores them in model objects and then to the database
+"""
 def populate(path_localdata,obj):
     author=""
     title=""
     desc=""
     date=""
     s=""
-    typeObj=type(obj)
-    print typeObj
+    typeObj=type(obj)#Find which type of model we are populating
     try:
-        no=len(os.walk(path_localdata).next()[2])
+        no=len(os.walk(path_localdata).next()[2])#find no. of json files in the directory
         for i in range(1,no+1):
-            with open(path_localdata+"//"+str(i)+".json") as data_file:
-                data=json.load(data_file)
-                t=typeObj()
-                t.save()
+            with open(path_localdata+"//"+str(i)+".json") as data_file:#open the json file
+                data=json.load(data_file)#load data in python variable
+                t=typeObj()#create a new object
+                t.save()#initially save the empty object. This generates a primary key which is required to add any new properties
                 for name in data:
                     if (name=='tags'):
                         for items in data[name]:
                             s1=str(items)
-                            t.tags.add(s1)
+                            t.tags.add(s1)#Add tags one by one.Note that this handles duplicate tags automatically
                     else:
-                        s=data[name]
+                        s=data[name]#Find the field
                         if(name=='author'):
                             author=s
                         elif(name=='title'):
@@ -50,16 +57,16 @@ def populate(path_localdata,obj):
                             desc=s
                         elif(name=='date_published'):
                             date=s
-                t.title=title
-                t.body=desc
+                t.title=title#Set the title
+                t.body=desc#Add the body
                 try:
-                    t.slug=slugify(title)
-                    t.save()
+                    t.slug=slugify(title)#Generate slug
+                    t.save()#Save the current state
                 except:
-                    t.slug=slugify(title)+str(random.randrange(1,10000000+1))
-                t.published=datetime.datetime.strptime(date,"%Y-%m-%d")
-                u=User.objects.get_or_create(username=author)
-                c=Category.objects.get_or_create(title='Uncategorized')
+                    t.slug=slugify(title)+str(random.randrange(1,10000000+1))#If 2 objects have same title,this prevents the same slug from being generated
+                t.published=datetime.datetime.strptime(date,"%Y-%m-%d") #Add publishing date
+                u=User.objects.get_or_create(username=author)#If user is absent, create user
+                c=Category.objects.get_or_create(title='Uncategorized')#If category is absent ,create category
                 t.save()
                 if(u[1]==True):
                     u[0].save()
@@ -71,7 +78,7 @@ def populate(path_localdata,obj):
                     t.authors=u[0]
                 t.categories.add(c[0])
                 t.save()
-                print("Entered element!")
+                print("Entered a element successfully in DB!")
     except IOError:
         print("IOError encountered!")
 
@@ -94,7 +101,3 @@ if __name__ == '__main__':
     if opt=='y':
         obj=WYSIWYGInput()
         populate(path_localdata=os.path.join(base,'Resources and Tools'),obj=obj)
-    """opt=raw_input("Do you want to populate the code snippets? (y/n):")
-    if opt=='y':
-        obj=WYS
-        populate(path_localdata=os.path.join(base,'Tutorials'))"""
