f97ea396b229533df260a9a6aa2ad279ae37280a
amanXCalibre <amanjhunjhunwala@outlook.com>
2015-05-29 20:30:32 +0530

    Cleaning up population script

--- astropython/populate.py
+++ astropython/populate.py
@@ -35,59 +35,57 @@ def populate(path_localdata,obj):
     date=""
     s=""
     typeObj=type(obj)#Find which type of model we are populating
-    try:
-        no=len(os.walk(path_localdata).next()[2])#find no. of json files in the directory
-        for i in range(1,no+1):
-            with open(path_localdata+"//"+str(i)+".json") as data_file:#open the json file
-                data=json.load(data_file)#load data in python variable
-                t=typeObj()#create a new object
-                t.save()#initially save the empty object. This generates a primary key which is required to add any new properties
-                for name in data:
-                    if (name=='tags'):
-                        for items in data[name]:
-                            s1=str(items)
-                            t.tags.add(s1)#Add tags one by one.Note that this handles duplicate tags automatically
-                    else:
-                        s=data[name]#Find the field
-                        if(name=='author'):
-                            author=s
-                        elif(name=='title'):
-                            title=s
-                        elif(name=='description'):
-                            desc=s
-                        elif(name=='date_published'):
-                            date=s
-                t.title=title#Set the title
-                t.body=desc#Add the body
-                try:
-                    t.slug=slugify(title)#Generate slug
-                    t.save()#Save the current state
-                except:
-                    t.slug=slugify(title)+str(random.randrange(1,10000000+1))#If 2 objects have same title,this prevents the same slug from being generated
-                t.published=datetime.datetime.strptime(date,"%Y-%m-%d") #Add publishing date
-                u=User.objects.get_or_create(username=author)#If user is absent, create user
-                c=Category.objects.get_or_create(title='Uncategorized')#If category is absent ,create category
-                t.save()
-                if(u[1]==True):
-                    u[0].save()
-                if(c[1]==True):
-                    c[0].save()
-                if(typeObj==type(WYSIWYGTutorial)):
-                    t.authors.add(u[0])
+    no=len(os.walk(path_localdata).next()[2])#find no. of json files in the directory
+    for i in range(1,no+1):
+        with open(path_localdata+"//"+str(i)+".json") as data_file:#open the json file
+            data=json.load(data_file)#load data in python variable
+            t=typeObj()#create a new object
+            t.save()#initially save the empty object. This generates a primary key which is required to add any new properties
+            for name in data:
+                if (name=='tags'):
+                    for items in data[name]:
+                        s1=str(items)
+                        t.tags.add(s1)#Add tags one by one.Note that this handles duplicate tags automatically
                 else:
-                    t.authors=u[0]
-                t.categories.add(c[0])
-                t.save()
-                print("Entered a element successfully in DB!")
-    except IOError:
-        print("IOError encountered!")
+                    s=data[name]#Find the field
+                    if(name=='author'):
+                        author=s
+                    elif(name=='title'):
+                        title=s
+                    elif(name=='description'):
+                        desc=s
+                    elif(name=='date_published'):
+                        date=s
+            t.title=title#Set the title
+            t.body=desc#Add the body
+            try:
+                t.slug=slugify(title)#Generate slug
+                t.save()#Save the current state
+            except:
+                t.slug=slugify(title)+str(random.randrange(1,10000000+1))#If 2 objects have same title,this prevents the same slug from being generated
+            t.published=datetime.datetime.strptime(date,"%Y-%m-%d") #Add publishing date
+            u=User.objects.get_or_create(username=author)#If user is absent, create user
+            c=Category.objects.get_or_create(title='Uncategorized')#If category is absent ,create category
+            t.save()
+            if(u[1]==True):
+                u[0].save()#If the user is not present ,create it
+            if(c[1]==True):
+                c[0].save()#If category is not present ,add it
+            if(typeObj==type(WYSIWYGTutorial)):
+                t.authors.add(u[0])
+            else:
+                t.authors=u[0]
+            t.categories.add(c[0])#Add categories
+            t.save()#save all elements
+            print("Entered an element successfully!")
 
 # Start execution here!
 if __name__ == '__main__':
     _temp_orig_path=os.getcwd()
     _orig_path=os.path.dirname(_temp_orig_path)
-    base=os.path.join(_orig_path,'ported_data/html')
+    base=os.path.join(_orig_path,'ported_data/html')#Path to Json data
     initialize()
+    #Take inputs
     print "Starting Astropython population script..."
     opt=raw_input("Do you want to populate the tutorials? (y/n):")
     if opt=='y':
