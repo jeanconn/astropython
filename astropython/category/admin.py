1d4ad477201ae5e5aaa177523f02c1567caebd04
amanXCalibre <amanjhunjhunwala@outlook.com>
2015-05-29 17:31:48 +0530

    Population Script Final Completion - Tutorials

--- astropython/category/admin.py
+++ astropython/category/admin.py
@@ -6,6 +6,7 @@ from category.models import Category
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_display = ('title', 'subtitle', 'slug')
+    list_filter = ('sites',)
 
 
 admin.site.register(Category, CategoryAdmin)
