6237361dcff1554610c12daf4ef4b7b33ec6eff7
amanXCalibre <amanjhunjhunwala@outlook.com>
2015-07-16 16:06:10 +0530

    closes #48

--- astropython/main/moderator.py
+++ astropython/main/moderator.py
@@ -8,8 +8,7 @@ from .models import *
 class Moderator(GenericModerator):
     fields_exclude=['updated','hits']
     visible_until_rejected=True
-    auto_approve_for_groups=['Trusted Users','Moderators']
-    auto_reject_for_groups=['Banned Users']
+    auto_approve_for_groups=['*']
 
 
     def is_auto_approve(self, obj, user):
