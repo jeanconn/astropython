2be2abeec8cf251a84c589dc95e0b1b2aad093ac
amanXCalibre <amanjhunjhunwala@outlook.com>
2015-07-19 23:56:39 +0530

    Minor bug !

--- astropython/main/signals.py
+++ astropython/main/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save,post_init
 from django.dispatch import receiver
 from django.contrib.auth.models import User,Group
 from moderation import signals
@@ -21,13 +21,14 @@ def moderation_approval(sender,instance,status,**kwargs):
         print "No Email Provided"
 
 
-@receiver(signals.post_moderation,dispatch_uid="add_content")
-def add_content(sender,instance,**kwargs):
-    try:
-        subject="New AstroPython Post!"
-        message="New post at AstroPython"
-        html_m='<hr /><h2 style="text-align:center"><code><tt><span style="font-family:trebuchet ms,helvetica,sans-serif"><strong>AstroPython - Python for Astronomers</strong></span></tt></code></h2><hr /><p>A Post "'+instance.title+'"&nbsp;&nbsp;has&nbsp;been added. It has been auto-approved (preview-phase bypassing) !</p><p>Access it <a href="http://amanjhunjhunwala.pythonanywhere.com'+instance.get_absolute_url()+'">here</a> !</p><p>Thank you,</p><p><strong>AstroPython Team</strong></p><hr /><p style="text-align:right"><span style="font-size:10px">Currently , you cannot unsubscribe to these emails</span></p>'
-        from_email="notifications@astropython.org"
-        send_mail(subject,message,from_email,["amanjjw@gmail.com"], fail_silently=False,html_message=html_m)
-    except:
-        print "No Email Provided"
+@receiver(post_save,dispatch_uid="add_content")
+def add_content(sender,instance,created,**kwargs):
+    if created==True:
+        try:
+            subject="New AstroPython Post!"
+            message="New post at AstroPython"
+            html_m='<hr /><h2 style="text-align:center"><code><tt><span style="font-family:trebuchet ms,helvetica,sans-serif"><strong>AstroPython - Python for Astronomers</strong></span></tt></code></h2><hr /><p>A Post "'+instance.title+'"&nbsp;&nbsp;has&nbsp;been added. It has been auto-approved (preview-phase bypassing) !</p><p>Access it <a href="http://amanjhunjhunwala.pythonanywhere.com'+instance.get_absolute_url()+'">here</a> !</p><p>Thank you,</p><p><strong>AstroPython Team</strong></p><hr /><p style="text-align:right"><span style="font-size:10px">Currently , you cannot unsubscribe to these emails</span></p>'
+            from_email="notifications@astropython.org"
+            send_mail(subject,message,from_email,["amanjjw@gmail.com"], fail_silently=False,html_message=html_m)
+        except:
+            print "No Email Provided"
