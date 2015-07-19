6c88a6f577a78a42fa86af3ddb667ed2e42f9efd
amanXCalibre <amanjhunjhunwala@outlook.com>
2015-07-19 15:49:11 +0530

    bugs fixed

--- astropython/main/signals.py
+++ astropython/main/signals.py
@@ -10,18 +10,24 @@ def add_to_preview(sender,instance,**kwargs):
 
 @receiver(signals.post_moderation)
 def moderation_approval(sender,instance,status,**kwargs):
-    if instance.state=="submitted":
-        subject="AstroPython Post Approved !"
-        message="Your post at AstroPython is approved"
-        html_m='<hr /><h2 style="text-align:center"><code><tt><span style="font-family:trebuchet ms,helvetica,sans-serif"><strong>AstroPython - Python for Astronomers</strong></span></tt></code></h2><hr /><p>Your Post "'+instance.title+'"&nbsp;&nbsp;has&nbsp;been approved by the Moderators !</p><p>Access it <a href="http://amanjhunjhunwala.pythonanywhere.com'+instance.get_absolute_url()+'">here</a> !</p><p>Thank you,</p><p><strong>AstroPython Team</strong></p><hr /><p style="text-align:right"><span style="font-size:10px">Currently , you cannot unsubscribe to these emails</span></p>'
-        from_email="notifications@astropython.org"
-        send_mail(subject,message,from_email,[str(user.email) for user in instance.authors.all()], fail_silently=False,html_message=html_m)
+    try:
+        if instance.state=="submitted":
+            subject="AstroPython Post Approved !"
+            message="Your post at AstroPython is approved"
+            html_m='<hr /><h2 style="text-align:center"><code><tt><span style="font-family:trebuchet ms,helvetica,sans-serif"><strong>AstroPython - Python for Astronomers</strong></span></tt></code></h2><hr /><p>Your Post "'+instance.title+'"&nbsp;&nbsp;has&nbsp;been approved by the Moderators !</p><p>Access it <a href="http://amanjhunjhunwala.pythonanywhere.com'+instance.get_absolute_url()+'">here</a> !</p><p>Thank you,</p><p><strong>AstroPython Team</strong></p><hr /><p style="text-align:right"><span style="font-size:10px">Currently , you cannot unsubscribe to these emails</span></p>'
+            from_email="notifications@astropython.org"
+            send_mail(subject,message,from_email,[str(user.email) for user in instance.authors.all()], fail_silently=False,html_message=html_m)
+    except:
+        print "No Email Provided"
 
 
 @receiver(signals.post_moderation)
 def add_content(sender,instance,**kwargs):
+    try:
         subject="New AstroPython Post!"
         message="New post at AstroPython"
         html_m='<hr /><h2 style="text-align:center"><code><tt><span style="font-family:trebuchet ms,helvetica,sans-serif"><strong>AstroPython - Python for Astronomers</strong></span></tt></code></h2><hr /><p>A Post "'+instance.title+'"&nbsp;&nbsp;has&nbsp;been added. It has been auto-approved (preview-phase bypassing) !</p><p>Access it <a href="http://amanjhunjhunwala.pythonanywhere.com'+instance.get_absolute_url()+'">here</a> !</p><p>Thank you,</p><p><strong>AstroPython Team</strong></p><hr /><p style="text-align:right"><span style="font-size:10px">Currently , you cannot unsubscribe to these emails</span></p>'
         from_email="notifications@astropython.org"
         send_mail(subject,message,from_email,["amanjjw@gmail.com"], fail_silently=False,html_message=html_m)
+    except:
+        print "No Email Provided"
